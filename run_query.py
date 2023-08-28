bf89a14869e25669d7ec17b34eabfdb56e132a6b
Alexandre Prates <alexandre.b.prates@gmail.com>
2023-08-28 00:12:05 -0300

    Add files via upload

---
+++ run_query.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+from google.cloud import bigquery
+
+# Construct a BigQuery client object.
+
+project = os.environ.get("PROJECT_ID")
+folder = sys.argv[2]
+
+client = bigquery.Client(project=project)
+
+files = os.listdir(folder)
+
+for file_name in files:
+    with open(os.path.join(folder, file_name)) as f:
+        query = f.read()
+        query_job = client.query(query)  # Make an API request.
+
